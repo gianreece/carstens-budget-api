7bf19f7159e730a7bd9ce3b7b27adefe640b1f0a
gianreece <gianreece@gmail.com>
2023-11-25 10:02:47 +0200

    fileupload

---
+++ src/main/java/com/carstensbudget/carstensBudget/CarstensBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.carstensbudget.carstensBudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarstensBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarstensBudgetApplication.class, args);
+	}
+
+}
